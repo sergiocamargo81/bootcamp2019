a4fbeb357614971e1d4fa134dbd793b8a7b17f7c
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-10-23 15:24:56 -0300

    Modulo02 - Added ESLint, Prettier & EditorConfig

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: "Hello rock!"});
-})
+  return res.json({ message: 'Hello Rocketseat!' });
+});
 
 export default routes;
