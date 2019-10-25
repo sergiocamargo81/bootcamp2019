01373a23b218dea0fa5900d61f893eecbeb4b3cc
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-10-25 00:01:54 -0300

    Modulo02 - Added loader for User model

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Rocketseat!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Sérgio Camargo',
+    email: 'sergiocamargo81@gmail.com',
+    password_hash: '1234567890',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
