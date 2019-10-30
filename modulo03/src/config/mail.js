87387b37b3e238a1f38803f9a3380f877f7f7a32
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-10-30 15:33:47 -0300

    Modulo03 - Added Nodemailer

---
+++ modulo03/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0bfb703751fc29',
+    pass: '137ea73f6c3a4f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
