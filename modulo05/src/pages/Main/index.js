160f50652912fa7e539991c616f02177d4b28527
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-11 11:27:13 -0300

    modulo05 - stylezed main

--- modulo05/src/pages/Main/index.js
+++ modulo05/src/pages/Main/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
 
-function Main() {
-  return <Title error={false}>Hello World</Title>;
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithub />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
-
-export default Main;
