60dacacf3a2f81795f3ffa4daec88e8dfe68c669
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-15 10:40:43 -0300

    modulo06 - reactotron

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import './config/ReactotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#7159c1',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
+    color: '#FFFFFF',
     margin: 10,
   },
 });
+
+console.tron.log('hello world');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.welcome}>Abaixo</Text>
+      <Text style={styles.welcome}>Spread Your Fire!!!</Text>
     </View>
   );
 }
