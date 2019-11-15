027effa83581c7ec668c0232068a8172deda1bba
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-15 11:25:10 -0300

    modulo06 - react navigation

--- modulo06/src/index.js
+++ modulo06/src/index.js
@@ -1,31 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#7159c1',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#FFFFFF',
-    margin: 10,
-  },
-});
-
-console.tron.log('hello world');
+import Routes from './routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.welcome}>Spread Your Fire!!!</Text>
-    </View>
-  );
+  return <Routes />;
 }
