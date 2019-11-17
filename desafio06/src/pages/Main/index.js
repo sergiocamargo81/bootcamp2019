d8c104ae3490261fcbc94db7180b012e1027194f
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-17 15:33:17 -0300

    desafio06 - repos loading

--- desafio06/src/pages/Main/index.js
+++ desafio06/src/pages/Main/index.js
@@ -39,6 +39,8 @@ export default class Main extends Component {
   };
 
   async componentDidMount() {
+    // AsyncStorage.clear();
+
     const users = await AsyncStorage.getItem('users');
 
     if (users) {
