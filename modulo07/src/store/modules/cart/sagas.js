995bb1c62794e99aec79dd8313085b3633eec5ac
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-24 11:58:11 -0300

    modulo07 - navigation on saga

--- modulo07/src/store/modules/cart/sagas.js
+++ modulo07/src/store/modules/cart/sagas.js
@@ -2,6 +2,7 @@ import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 
 import api from '../../../services/api';
+import history from '../../../services/history';
 import { formatPrice } from '../../../util/format';
 
 import { addToCartSucess, updateAmountSuccess } from './actions';
@@ -35,6 +36,8 @@ function* addToCart({ id }) {
     };
 
     yield put(addToCartSucess(data));
+
+    history.push('/cart');
   }
 }
 
