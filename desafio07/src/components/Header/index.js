edb18b455f55974bc783905332dbadde003862f0
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-28 17:25:40 -0300

    desafio07 - header

--- desafio07/src/components/Header/index.js
+++ desafio07/src/components/Header/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import {View} from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Header() {
-  return <View />;
+import {Wrapper, Container, Logo, BasketContainer, ItemCount} from './styles';
+
+export default function Header({navigation, cartSize}) {
+  return (
+    <Wrapper>
+      <Container>
+        <Logo onPress={() => navigation.navigate('Home')} />
+        <BasketContainer onPress={() => navigation.navigate('Cart')}>
+          <Icon name="shopping-basket" color="#FFF" size={30} />
+          <ItemCount>{cartSize || 0}</ItemCount>
+        </BasketContainer>
+      </Container>
+    </Wrapper>
+  );
 }
