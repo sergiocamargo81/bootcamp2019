2ec9cf8b5fe1c5a8aaa6dafba4503fde60c45a09
Sérgio Camargo <sergiocamargo81@gmail.com>
2019-11-28 17:48:40 -0300

    desafio07 - styles changed

--- desafio07/src/components/Header/index.js
+++ desafio07/src/components/Header/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import {TouchableOpacity} from 'react-native';
+
 import {Wrapper, Container, Logo, BasketContainer, ItemCount} from './styles';
 
 export default function Header({navigation, cartSize}) {
   return (
     <Wrapper>
       <Container>
-        <Logo onPress={() => navigation.navigate('Home')} />
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+          <Logo />
+        </TouchableOpacity>
         <BasketContainer onPress={() => navigation.navigate('Cart')}>
           <Icon name="shopping-basket" color="#FFF" size={30} />
           <ItemCount>{cartSize || 0}</ItemCount>
--- desafio07/src/pages/Home/index.js
+++ desafio07/src/pages/Home/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 import {Container} from './styles';
 
 export default function Home() {
-  return <Container />;
+  return (
+    <Container>
+      <Text style={{color: '#FFF'}}>Home</Text>
+    </Container>
+  );
 }
 
 Home.navigationOptions = {
